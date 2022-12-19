34b7b1051eb197af3f3ce1cf614cfda0360ca069
gitfirstlesson <metin5153@gmail.com>
2022-12-19 11:41:42 +0000

    Automation first

---
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigation.java
@@ -0,0 +1,45 @@
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigation {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver= new ChromeDriver();
+
+        driver.get("https://tesla.com");
+
+        Thread.sleep(3000);
+        driver.getTitle();
+        System.out.println(" driver.getTitle()="+driver.getTitle());
+        driver.navigate().back();
+
+        Thread.sleep(3000);
+
+        driver.navigate().forward();
+
+        Thread.sleep(3000);
+
+        driver.navigate().refresh();
+        Thread.sleep(3000);
+        driver.navigate().to("https://www.google.com");
+        String currentURL=driver.getCurrentUrl();
+        System.out.println("currentURL"+currentURL);
+
+
+    }
+
+
+
+
+
+
+
+
+}
