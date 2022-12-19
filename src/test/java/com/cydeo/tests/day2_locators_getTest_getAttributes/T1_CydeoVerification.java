7e1e853c54cb582c213abcc55549d373151ff11f
gitfirstlesson <metin5153@gmail.com>
2022-12-19 16:05:19 +0000

    day2 task2

--- src/test/java/com/cydeo/tests/day2_locators_getTest_getAttributes/T1_CydeoVerification.java
+++ src/test/java/com/cydeo/tests/day2_locators_getTest_getAttributes/T1_CydeoVerification.java
@@ -1,7 +1,46 @@
 package com.cydeo.tests.day2_locators_getTest_getAttributes;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T1_CydeoVerification {
     public static void main(String[] args) {
+
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver= new ChromeDriver();
+
+        driver.manage().window().maximize();
+       // driver.get("https://practice.cydeo.com");
+        driver.navigate().to("https://practice.cydeo.com");
+
+        String expectedURL="cydeo";
+
+        String actualURL= driver.getCurrentUrl();
+         if(actualURL.contains(expectedURL)){
+             System.out.println("URL VERIFICATION PASSED");
+         }else {
+             System.out.println("URL VERIFICATION FAILED");
+         }
+
+
+
+         String expectedTitle=" Practice";
+
+         String actualTitle=driver.getTitle();
+
+         if(expectedTitle.equals(actualTitle)){
+             System.out.println("tittle verification passed");
+         }else {
+             System.out.println("failed");
+         }
+
+         driver.close();
+
+
+
 
 
 
---
+++ src/test/java/com/cydeo/tests/day2_locators_getTest_getAttributes/T2_LinkTextPractice.java
@@ -0,0 +1,29 @@
+package com.cydeo.tests.day2_locators_getTest_getAttributes;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_LinkTextPractice {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromiumdriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
