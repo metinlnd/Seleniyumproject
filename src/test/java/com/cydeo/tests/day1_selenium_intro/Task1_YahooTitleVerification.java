fd398879edd476253889bfddbdfa9736b92596b8
gitfirstlesson <metin5153@gmail.com>
2022-12-19 15:14:50 +0000

    day2 task1 CydeoVerification

--- src/test/java/com/cydeo/tests/day1_selenium_intro/Task1_YahooTitleVerification.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/Task1_YahooTitleVerification.java
@@ -1,13 +1,34 @@
 package com.cydeo.tests.day1_selenium_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task1_YahooTitleVerification {
     public static void main(String[] args) {
 
 
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver= new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://www.yahoo.com");
+
+
+String expectedTitle="Yahoo UK | News, email and search";
+
+String actualTitle= driver.getTitle();
+
+if(actualTitle.equals(expectedTitle)){
+    System.out.println("Title is expected. verifaciton passed");
+}else {
+    System.out.println("verifation failed");
 
 
 
-
+}
 
 
 
---
+++ src/test/java/com/cydeo/tests/day2_locators_getTest_getAttributes/T1_CydeoVerification.java
@@ -0,0 +1,38 @@
+package com.cydeo.tests.day2_locators_getTest_getAttributes;
+
+public class T1_CydeoVerification {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
