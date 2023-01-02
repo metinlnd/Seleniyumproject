2d99fbb1124841d4bb1c8bee6c56fbe9288a0543
gitfirstlesson <metin5153@gmail.com>
2023-01-02 14:15:39 +0000

    day5 checked boxes

--- src/test/java/com/cydeo/tests/day4_findelement_css_xpath/T4_FindElement.java
+++ src/test/java/com/cydeo/tests/day4_findelement_css_xpath/T4_FindElement.java
@@ -1,8 +1,11 @@
 package com.cydeo.tests.day4_findelement_css_xpath;
 
 import com.cydeo.utility.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class T4_FindElement {
@@ -16,11 +19,14 @@ public class T4_FindElement {
 
         driver.get("https://practice.cydeo.com/abtest");
 
+        List<WebElement> alllinks = driver.findElements(By.tagName("a"));
+        System.out.println(alllinks.size());
 
+        for (WebElement each : alllinks) {
+            System.out.println("text of link:"+ each.getText());
 
-
-
-
+            System.out.print("HREF VALUE:"+ each.getAttribute("href"));
+        }
 
 
 
---
+++ src/test/java/com/cydeo/tests/day4_findelement_css_xpath/T5_checked_boxes.java
@@ -0,0 +1,4 @@
+package com.cydeo.tests.day4_findelement_css_xpath;
+
+public class T5_checked_boxes {
+}
