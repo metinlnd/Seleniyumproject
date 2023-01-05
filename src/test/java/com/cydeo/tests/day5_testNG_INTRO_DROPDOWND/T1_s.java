2a2a5a057de836952567c3239060eb2e6d199064
gitfirstlesson <metin5153@gmail.com>
2023-01-05 14:10:33 +0000

    day6 checked boxes

--- src/test/java/com/cydeo/tests/day5_testNG_INTRO_DROPDOWND/T1_s.java
+++ src/test/java/com/cydeo/tests/day5_testNG_INTRO_DROPDOWND/T1_s.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day5_testNG_INTRO_DROPDOWND;
 
 import com.cydeo.utility.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,14 +16,27 @@ public class T1_s {
         driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
         driver.get("https://practice.cydeo.com/add_remove_elements/");
 
+        WebElement addElementbuttom= driver.findElement(By.xpath("//button [@onclick='addElement()']"));
+
+ //System.out.println(addElementbuttom.isDisplayed());;
+
+       // System.out.println(addElementbuttom.isSelected());
+//
+       // System.out.println(addElementbuttom.getText());
+        addElementbuttom.click();
+
+WebElement deletebuttom= driver.findElement(By.xpath("//button[@class='added-manually']"));
+
+deletebuttom.click();
+
+try { System.out.println(deletebuttom.isDisplayed());
+
+}catch (StaleElementReferenceException e){
+    System.out.println("STALEELEMENT REFERNCE");
+};
 
 
-
-
-
-
-
-
+driver.close();
 
 
 
